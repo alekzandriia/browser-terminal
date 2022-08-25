165aa4b995c5410b23b13289317138a1926692f4
Alekzandriia <alekzoreillyz@me.com>
2022-08-25 16:04:08 -0400

    Added a Skip Animation Function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 $('#main').t({
     delay: 0,//start delay (s)
-    speed: 30,//typing speed in ms (min: 10)
+    speed: 20,//typing speed in ms (min: 10)
     beep: true,
     caret: '<span style="color:hotpink; font-size:24px;">❤︎</span>',
+    blink: true,
+    blink_perm:false,
     typing: function (elm, chr) {
         if (chr.match(/\-trigger/))
             $('#pow-txt').show().delay(500).fadeOut(0);
+    },
+    fin:function(elm){
+        $('#skip').hide();
     }
 })
+
+
+
+
+function skipIntro() {
+console.log('hello')
+$('#main').hide();
+$('#skipped').show();
+$('#skip').hide();
+}
+
+
+
+
+
+
